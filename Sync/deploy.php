1c7a3dcffbd9b93ea8cb11c677feb432395144c1
Juan Alcántara <jamespond402@gmail.com>
2017-03-26 19:58:13 +0200

    Create deploy.php

---
+++ Sync/deploy.php
@@ -0,0 +1,37 @@
+<?php
+   
+    $commands = array(
+        'echo $PWD',
+        'whoami',
+        'git pull',
+        'git status',
+        'git submodule sync',
+        'git submodule update',
+        'git submodule status',
+    );
+
+    // exec commands
+    $output = '';
+    foreach($commands AS $command){
+        $tmp = shell_exec($command);
+        
+        $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
+        $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
+    }
+?>
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>Actualizando la pagina web</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<div style="width:700px">
+    <div style="float:left;width:350px;">
+    <p style="color:white;">Actualizando con los comandos</p>
+    <?php echo $output; ?>
+    </div>
+</div>
+</body>
+</html>

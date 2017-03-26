ed5b50137550e97e99225b8541da11dfd9c37915
Juan Alcántara <jamespond402@gmail.com>
2017-03-26 21:08:10 +0200

    Update deploy.php

--- Sync/deploy.php
+++ Sync/deploy.php
@@ -1,14 +1,6 @@
 <?php
    
-    $commands = array(
-        'echo $PWD',
-        'whoami',
-        'git pull',
-        'git status',
-        'git submodule sync',
-        'git submodule update',
-        'git submodule status',
-    );
+    $commands = array('git checkout -f', 'git pull GameSystem HEAD'); // Los dos comandos git que se ejecutan
 
     // exec commands
     $output = '';
